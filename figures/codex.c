21656cdad83fe6b93928518b4352e330cb1afba6
Andrew Tran <atran@gmail.com>
2018-06-05 20:36:34 -0700

    added the example alias analyses

--- figures/codex.c
+++ figures/codex.c
@@ -1,19 +1,19 @@
-void foo() {
+void foo() {		Andersen		Steensgaard
    int a, b, c;
    int *x, *y, *z;
 
-   x = &a;
-   y = &b;
-   z = y;
+   x = &a;		x : {a}			x : {a}
+   y = &b;		y : {b}			y : {b}
+   z = y;		z : {b}			z : {b}
    c = 0;
    *x = c;
 
    if (c > 1) {
-      y = z - 4;
+      y = z - 4;	y : {b, unknown}	y : {b}
    }
    else {
-      y = x - &a;
+      y = x - &a;	y : {b, unknown}	y : {a, b}
    }
 
-   z = x;
+   z = x;		z : {a, b}		z : {a, b}
 }
